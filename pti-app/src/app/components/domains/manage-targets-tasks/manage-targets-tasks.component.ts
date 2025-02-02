2fd0d33c02a7d621875c38037b4587bbd5d19c13
Wolfrine <schttewary@gmail.com>
2025-02-02 13:34:00 +0530

    Dashboard template update

---
+++ pti-app/src/app/components/domains/manage-targets-tasks/manage-targets-tasks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-targets-tasks',
+  imports: [],
+  templateUrl: './manage-targets-tasks.component.html',
+  styleUrl: './manage-targets-tasks.component.scss'
+})
+export class ManageTargetsTasksComponent {
+
+}

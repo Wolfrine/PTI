094f801ef0c0a59a53587f77dba16cf222cb3ab1
Wolfrine <schttewary@gmail.com>
2026-09-16 15:28:43 +0530

    Cache PIN/session bootstrap in Velum PWA

--- velum-gallery/sw.js
+++ velum-gallery/sw.js
@@ -1,5 +1,5 @@
-const CACHE='velum-shell-v2';
-const SHELL=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icons/icon.svg'];
+const CACHE='velum-shell-v3';
+const SHELL=['./','./index.html','./styles.css','./bootstrap.js','./app.js','./manifest.webmanifest','./assets/icons/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{

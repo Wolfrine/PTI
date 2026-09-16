1267ae136a26a946917700eb38551500d56b2209
Wolfrine <schttewary@gmail.com>
2026-09-16 16:34:30 +0530

    Bump Velum immersive Drift PWA shell

--- velum-gallery/sw.js
+++ velum-gallery/sw.js
@@ -1,4 +1,4 @@
-const CACHE='velum-shell-v6';
+const CACHE='velum-shell-v7';
 const SHELL=['./','./index.html','./styles.css','./interaction-upgrades.js','./bootstrap.js','./app.js','./manifest.webmanifest','./assets/icons/icon.svg'];
 
 self.addEventListener('install',event=>{
